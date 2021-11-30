4224963d9f806c5cd4c0b4b812e43f5946546fb3
Vipin Meghwal <vipination@gmail.com>
2021-11-30 15:59:01 +0530

    crousal updated

--- client/src/Component/Utility/Crousal.js
+++ client/src/Component/Utility/Crousal.js
@@ -3,7 +3,9 @@ import '../../../node_modules/@glidejs/glide/dist/css/glide.core.min.css'
 import '../../../node_modules/@glidejs/glide/dist/css/glide.theme.min.css'
 import Glide from '@glidejs/glide'
 import '../../Assets/CSS/Crousal.css'
+import crosalPic from '../../Assets/Images/crousalPic.jpg'
 import kustomParts from '../../Assets/Images/bike_art_kustomParts.jpg'
+import { NavLink } from 'react-router-dom'
 
 
 const Crousal = () => {
@@ -34,22 +36,25 @@ const Crousal = () => {
                         <div className="block h-50vh mobile-height-crousal w-full mx-auto flex pt-6 md:pt-0 md:items-center bg-cover bg-right  " >
                             <div className="container mx-auto">
                                 <div className="flex flex-col w-full lg:w-1/2 md:ml-16 items-center md:items-start px-16  tracking-wide" >
-                                    <img src={kustomParts}   style={{ maxWidth: "150%" }} />
+                                    <NavLink exact to='/product/Vega-Bolt-Rapid-white-blue-Helmet'>
+                                        <img src={crosalPic} style={{ maxWidth: "150%" }} />
+                                    </NavLink>
                                 </div>
                             </div>
                         </div>
                     </li>
-                    {/* <li className="glide__slide " >
+                    <li className="glide__slide " >
 
-                        <div className="block h-50vh w-full mx-auto flex pt-6 md:pt-0 md:items-center bg-cover bg-right backgroundImage3" >
+                        <div className="block h-50vh mobile-height-crousal w-full mx-auto flex pt-6 md:pt-0 md:items-center bg-cover bg-right  " >
                             <div className="container mx-auto">
                                 <div className="flex flex-col w-full lg:w-1/2 md:ml-16 items-center md:items-start px-16  tracking-wide" >
-                                    <p className="text-black text-2xl my-4  ">Buy Book As Your Wish</p>
-                                    <a className="text-xl inline-block no-underline border-b border-gray-600 leading-relaxed hover:text-black hover:border-black" href="#"  >view product</a>
+                                    <img src={kustomParts} style={{ maxWidth: "150%" }} />
                                 </div>
                             </div>
                         </div>
-                    </li> */}
+                    </li>
+
+
 
 
 
